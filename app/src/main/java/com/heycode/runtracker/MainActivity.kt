d0424e5a5d5edc42e726f23954fdc6b51e0f2945
Kishan Kr Sharma <skishan781@gmail.com>
2021-05-05 15:06:17 +0530

    Dagger-Hilt setup done

--- app/src/main/java/com/heycode/runtracker/MainActivity.kt
+++ app/src/main/java/com/heycode/runtracker/MainActivity.kt
@@ -2,11 +2,19 @@ package com.heycode.runtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import com.heycode.runtracker.database.RunDAO
+import com.heycode.runtracker.database.RunDAO_Impl
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var runDAO: RunDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("KishanRunDAO","RUNDAO: "+runDAO.hashCode())
     }
 }
---
+++ app/src/main/java/com/heycode/runtracker/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.heycode.runtracker.utils
+
+object Constants {
+    const val RUNNING_DATABASE_NAME = "running_db"
+}
